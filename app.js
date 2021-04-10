fa4ddbc92d6581fb5b0effe1bfb28ef273ee6478
cheikh <bychec@gmail.com>
2021-04-10 21:33:47 +0200

    Mongoose + database created

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const Book = require('./models/book');
+
+// conect to the mongodb
+const dbURI='mongodb+srv://cheikh:al-Faruq4@books.sfd85.mongodb.net/learnedbook?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true})
+    .then((result)=>app.listen(3000))
+    .catch((err)=>console.log(err));
 
 const book=require('./router/book')
 
@@ -7,7 +16,7 @@ const book=require('./router/book')
 app.set('view engine','ejs');
 
 // listen for request 
-app.listen(3000);
+// app.listen(3000);
 
 //middleware & static files
 app.use(express.static('public'));
@@ -15,6 +24,19 @@ app.use(express.static('public'));
 app.get('/',(req,res)=>{
     
     res.render('home');
+})
+
+app.get('/add-book',(req,res)=>{
+    const book = new Book({
+        author:'dienf sylla',
+        bookName:'learn java',
+        finished:'20/02/2021'
+    });
+    book.save()
+        .then((result)=>{
+            res.send(result);
+        })
+        .catch((err)=>console.log(err));
 })
 
 // about page 
@@ -32,5 +54,11 @@ app.get('/about-me',(req,res)=>{
 })
 
 
-
+app.get('/all-book',(req,res)=>{
+    Book.findById('6071f88ca22e703248eac402')
+        .then(result=>{
+            res.send(result)
+        })
+        .catch(err=>console.log(err));
+})
 
--- router/book.js
+++ router/book.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+// const mongoose = require('mongoose');
+// // conect to the mongodb
+// const dbURI='mongodb+srv://cheikh:al-Faruq4@books.sfd85.mongodb.net/learnedbook?retryWrites=true&w=majority';
 
+// mongoose.connect(dbURI,{useNewUrlParser:true,useUnifiedTopology:true})
+//     .then((result)=>app.listen(3000))
+//     .catch((err)=>console.log(err));
 // setting the static middleware for this route 
 router.use(express.static('public'));
+
+
+
 
 router.get('/create',(req,res)=>{
     res.render('newbook');
